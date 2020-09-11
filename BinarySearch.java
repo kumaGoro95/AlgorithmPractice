e706c7a7066c9d7ac80d54145082cfba353a2a90
kumaGoro <todira.island@gmail.com>
2020-09-11 18:37:46 +0900

    Modify the BinarySearch file. #3

---
+++ BinarySearch.java
@@ -0,0 +1,28 @@
+public class BinarySearch{
+  public static void main(String [] args){
+    int[] numbers = {6,7,13,15,17,22,25,32,35,37,40,43,45,48};
+    int targetnum = 25;
+    int tail = 13;
+    int head = 0;
+    execute(numbers, targetnum, tail, head);
+  }
+
+  public static void execute(int[] numbers, int targetnum, int tail, int head){
+    int result = -1;
+    for(head = 0 ; head < tail ;){
+      int center = (head + tail) /2;
+      if (numbers[center] == targetnum){
+        System.out.println("index number is "+center);
+        result = 0;
+        break;
+      }else if(numbers[center] < targetnum){
+        head = center+1;
+      }else{
+        tail = center-1;
+      }
+    }
+    if(result == -1){
+      System.out.println("Not found");
+    }
+  }
+}
