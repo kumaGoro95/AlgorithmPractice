6c12de31321de5749d14fb5687c136ec291fe49c
kumaGoro <todira.island@gmail.com>
2020-09-11 14:34:50 +0900

    add file LinearSearch

---
+++ LinearSearch.java
@@ -0,0 +1,25 @@
+public class LinearSearch{
+  public static void main(String [] args){
+    int[] numbers = {4,5,9,1,3,8,7,2,6};
+    int targetnum = 6;
+    int result = searchExecute(numbers, targetnum);
+
+    if(result != -1){
+      System.out.println(targetnum+"'s index key ="+result);
+    }else{
+      System.out.println("Not found");
+    }
+  }
+
+  public static int searchExecute(int[] numbers, int targetnum){
+    int result = -1;
+    int nonumber = -1;
+    for(int i = 0; i < numbers.length; i++){
+      if(numbers[i] == targetnum){
+        result = i;
+        return result;
+      }
+    }
+    return nonumber;
+  }
+}
